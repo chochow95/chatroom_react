7e93b2eb8c608832233d1a6e3eac24cf9f2f2dc3
Cho Zhang <zhan5559@umn.edu>
2019-12-07 23:32:47 -0600

    First commit

---
+++ server/index.js
@@ -0,0 +1,61 @@
+const express = require('express');
+
+const socketio = require('socket.io');
+
+const http = require('http');
+
+const PORT = process.env.PORT || 5000;
+
+
+const router = require("./router");
+
+const { addUser, removeUser, getUser, getUsersInRoom } = require('./user');
+
+const app = express();
+
+const server= http.createServer(app);
+
+const io= socketio(server);
+io.on("connection", (socket) => {
+
+  console.log("new user log in");
+
+
+  socket.on('join', ({ name, room }, callback) => {
+
+    const{error,user} =addUser({ id: socket.id, name, room });
+
+    console.log(name, room);
+
+    if(error) return callback(error);
+
+
+    socket.join(user,room);
+    socket.emit('message', { user: 'admin', text: `${user.name}, welcome to room ${user.room}.`});
+    socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name} has joined!` });
+
+
+    callback();
+
+  });
+
+
+  socket.on('sendMessage', (message, callback) => {
+     const user = getUser(socket.id);
+
+     io.to(user.room).emit('message', { user: user.name, text: message });
+
+     callback();
+   });
+
+
+  socket.on("disconnect",() => {
+    console.log("use has lefe");
+  })
+
+
+});
+
+app.use(router);
+
+server.listen(process.env.PORT || 5000,() => console.log("server has start"));
